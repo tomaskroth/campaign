45e06fc162890809d09dd5b1093891d94fb87c16
Tomás Kroth <tomaskroth@gmail.com>
2019-02-06 22:11:11 +0100

    Spring Initializer

---
+++ src/main/java/tool/rpg/campaign/CampaignApplication.java
@@ -0,0 +1,14 @@
+package tool.rpg.campaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignApplication.class, args);
+	}
+
+}
+
